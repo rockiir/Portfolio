38f7df99ab6b0e89046a5e4b69826e83c2382b0f
rockiir <70121094+rockiir@users.noreply.github.com>
2023-10-30 12:28:49 -0300

    Alterações Portfolio

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function PageNotFound() {
   return (
     <div>
-      <h2>Página não encontrada</h2>
+      <h2>Página não encontrada!</h2>
       <p>A página que você está procurando não existe.</p>
     </div>
   );
