89f4f6a1af9b36ba19b1350e6eae6b8de0b3909b
rockiir <70121094+rockiir@users.noreply.github.com>
2023-10-29 22:41:38 -0300

    Alterações Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Contact from './pages/Contact';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './config/createTheme';
 import { IoLogoWhatsapp } from 'react-icons/io';
+import PageNotFound from './pages/PageNotFound';
 
 
 function App() {
@@ -24,6 +25,8 @@ function App() {
               <Route path="/About" element={<About />} />
               <Route path="/Contact" element={<Contact />} />
               <Route path="/Projects" element={<Projects />} />
+              <Route path="*" element={<PageNotFound />} />
+
             </Routes>
           </div>
         </ThemeProvider>
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageNotFound() {
+  return (
+    <div>
+      <h2>Página não encontrada</h2>
+      <p>A página que você está procurando não existe.</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
