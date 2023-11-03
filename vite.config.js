dc16a5479f6ffe383e539852c838416cde357330
rockiir <70121094+rockiir@users.noreply.github.com>
2023-11-02 21:09:11 -0300

    Alterações Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  plugins: [react()],
-  base:"/Portfolio",
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
 
-})
+  build: {
+    target: 'esnext',
+  },
+
+  plugins: [react],
+  base:"/Portfolio",
+});
